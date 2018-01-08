1a1a81fbef8a0fd63496b99424d6fe77de97f14c
dags- <dags@dags.me>
2018-01-08 23:52:46 +0000

    added post preview to application form

--- _public/form/form.js
+++ _public/form/form.js
@@ -40,6 +40,13 @@ function addSelector(form, type) {
     select.addEventListener("change", function () {
         var type = this.options[this.selectedIndex].value;
         newForm(type);
+
+        var colors = ["#00d56a", "#0080ff", "#ff8080"];
+        var preview = document.getElementById("pr-post");
+        preview.style.borderColor = colors[this.selectedIndex];
+
+        var header = document.getElementById("pr-header");
+        header.innerText = "#" + type.charAt(0).toUpperCase() + type.slice(1);
     });
 
     var section = document.createElement("section");
@@ -56,43 +63,103 @@ function addBody(form, type) {
         type: "text",
         maxlength: 120,
         desc: "What is your server/twitch/youtube name?"
+    }, function () {
+        var target = document.getElementById("pr-" + this.id);
+        target.innerText = this.value;
     }));
+
     form.appendChild(makeTextArea({
         name: "description",
         id: "description",
         type: "textarea",
         maxlength: 480,
         desc: "Describe what you're all about"
+    }, function () {
+        var target = document.getElementById("pr-" + this.id);
+        target.innerText = this.value;
     }));
+
     form.appendChild(makeInput({
         name: "icon",
         id: "icon",
         type: "text",
         maxlength: 240,
         desc: "Link an icon image to accompany your promotion (optional)"
+    }, function () {
+        var icon = document.getElementById("pr-icon");
+        var smallIcon = document.getElementById("pr-icon-small");
+        if (this.value === "") {
+            icon.style.display = "none";
+            smallIcon.style.display = "none";
+        } else {
+            icon.style.display = "block";
+            smallIcon.style.display = "inline-block";
+            icon.src = this.value;
+            smallIcon.src = this.value;
+        }
     }));
+
     form.appendChild(makeInput({
-        name: "media",
-        id: "media",
+        name: "image",
+        id: "image",
         type: "text",
         maxlength: 240,
         desc: "Link an image to accompany your promotion (optional)"
+    }, function () {
+        var image = document.getElementById("pr-image");
+        if (this.value === "") {
+            image.style.display = "none";
+        } else {
+            image.style.display = "block";
+            image.src = this.value;
+        }
     }));
+
     form.appendChild(makeInput({
-        name: "link",
-        id: "link",
+        name: "website",
+        id: "website",
         type: "text",
         maxlength: 240,
-        desc: "Got a website or discord you'd like people to visit? (optional)"
+        desc: "Got a website you'd like people to visit? (optional)"
+    }, function() {
+        var field = document.getElementById("pr-website-field");
+        var site = document.getElementById("pr-website");
+        if (this.value === "") {
+            field.style.display = "none";
+        } else {
+            field.style.display = "inline-block";
+            site.innerText = this.value;
+            site.href = this.value;
+        }
+    }));
+
+    form.appendChild(makeInput({
+        name: "discord",
+        id: "discord",
+        type: "text",
+        maxlength: 120,
+        desc: "Got a discord you'd like people to join? (optional)"
+    }, function() {
+        var field = document.getElementById("pr-discord-field");
+        var site = document.getElementById("pr-discord");
+        if (this.value === "") {
+            field.style.display = "none";
+        } else {
+            field.style.display = "inline-block";
+            site.href = this.value;
+        }
     }));
 
     switch (type) {
         case "server":
+            document.getElementById("pr-ip-field").style.display = "inline-block";
+            document.getElementById("pr-whitelist-field").style.display = "inline-block";
             addServer(form);
             break;
         case "twitch":
-            break;
         case "youtube":
+            document.getElementById("pr-ip-field").style.display = "none";
+            document.getElementById("pr-whitelist-field").style.display = "none";
             break;
     }
 }
@@ -108,17 +175,33 @@ function addServer(form) {
         id: "ip",
         type: "text",
         maxlength: "120",
-        desc: "What\"s the server\"s ip address?"
+        desc: "What's the server's ip address?"
+    }, function() {
+        var ip = document.getElementById("pr-ip");
+        if (this.value === "") {
+            ip.innerText = "required";
+        } else {
+            ip.innerText = this.value;
+        }
     }));
+
     form.appendChild(makeInput({
         name: "whitelist",
         id: "whitelist",
         type: "checkbox",
         desc: "Does your server use a whitelist?"
+    }, function () {
+        var whitelist = document.getElementById("pr-whitelist");
+        console.log(this.checked);
+        if (this.checked) {
+            whitelist.innerText = "Yes";
+        } else {
+            whitelist.innerText = "No";
+        }
     }));
 }
 
-function makeInput(props) {
+function makeInput(props, listener) {
     var section = document.createElement("section");
 
     if (props["desc"] !== undefined) {
@@ -128,6 +211,9 @@ function makeInput(props) {
     }
 
     var input = makeElement("input", props);
+    var event = props["type"] === "checkbox" ? "change" : "input";
+    input.addEventListener(event, listener);
+
     var current = document.getElementById(props['id']);
     if (current !== null) {
         input.innerText = current.innerText;
@@ -145,11 +231,12 @@ function makeElement(type, props) {
     return el;
 }
 
-function makeTextArea(props) {
+function makeTextArea(props, listener) {
     var label = document.createElement("label");
     label.innerText = props["desc"];
 
     var text = makeElement("textarea", props);
+    text.addEventListener("input", listener);
 
     var section = document.createElement("section");
     section.appendChild(label);
